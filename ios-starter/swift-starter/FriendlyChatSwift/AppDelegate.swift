1433af8069b0093c561cebe559aa6d3f81ba1304
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-04-26 20:24:09 +0900

    Set clientID for Google Sign In

--- ios-starter/swift-starter/FriendlyChatSwift/AppDelegate.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/AppDelegate.swift
@@ -45,6 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions
     launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FIRApp.configure()
+    GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
     GIDSignIn.sharedInstance().delegate = self
     return true
   }
