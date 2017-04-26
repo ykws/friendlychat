a993c21c0c69102768ef3173ece1a8b6a5aad6fa
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-04-26 23:57:38 +0900

    Initialize Measurment
    If you want to view this activity in your console, select Product
    ... Scheme ... Edit Scheme in Xcode. In the Arguments Passed on
    Launch section, click the + to add a new argument and add
    `-FIRAnalyticsDebugEnabled` as the new argument.

--- ios-starter/swift-starter/FriendlyChatSwift/MeasurementHelper.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/MeasurementHelper.swift
@@ -19,11 +19,14 @@ import Firebase
 class MeasurementHelper: NSObject {
 
   static func sendLoginEvent() {
+    FIRAnalytics.logEvent(withName: kFIREventLogin, parameters: nil)
   }
 
   static func sendLogoutEvent() {
+    FIRAnalytics.logEvent(withName: "logout", parameters: nil)
   }
 
   static func sendMessageEvent() {
+    FIRAnalytics.logEvent(withName: "message", parameters: nil)
   }
 }
